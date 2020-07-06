e7d13aa2a046e87aabbb274fd306e580cdc7a730
paulineketelaars <61013780+paulineketelaars@users.noreply.github.com>
2020-07-06 12:13:13 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+// References to html
+let myHTML = document.querySelector('html');
+let myImage = document.querySelector('img');
+let myHeading = document.querySelector('h1');
+let myButton = document.querySelector('button');
+
+// Change image function
+function clickFunction() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === "images/harry-potter.jpg") {
+      myImage.setAttribute ('src','images/PSV.jpg');
+    } else {
+      myImage.setAttribute ('src',"images/harry-potter.jpg");
+    }
+}
+
+myImage.onclick = clickFunction;
+
+//Username function
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if (!myName) {setUserName();
+  }else {	
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Programming is cool, ' + myName;
+  }
+}
+
+myButton.onclick = setUserName;
+
+//Initialization code
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
